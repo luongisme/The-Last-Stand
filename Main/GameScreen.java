77363fcdd3cd53836310e1194c4a05ba552d4043
VSon2906 <viet29son@gmail.com>
2025-10-08 15:29:16 +0700

    create panel

--- Main/GameScreen.java
+++ Main/GameScreen.java
@@ -1,4 +1,40 @@
 package Main;
-public class GameScreen {
-    
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+//import Input.MouseListener;
+
+public class GameScreen extends JPanel {
+    private Game game;
+    private Dimension size; // set size panel
+    // private MouseListener mouseListener; // mouse handling
+    private int sizeWidth = 1504;
+    private int sizeHeight = 736;
+
+    // ===== Constructor =====
+    public GameScreen(Game game) {
+        this.game = game;
+        setPanelSize();
+        // initInput();
+    }
+
+    // private void initInput(){}
+
+    private void setPanelSize() {
+        size = new Dimension(sizeWidth,sizeHeight); // create size of panel
+        setPreferredSize(size);
+        System.out.println("GameScreen: Panel size set to " + size.width + "x" + size.height);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0,sizeWidth,sizeHeight);
+        // g.getRender()
+    }
+
 }
+
