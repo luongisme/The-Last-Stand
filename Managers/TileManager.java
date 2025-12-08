b3d1175d0a1728105be6dd59ffabf725ede58387
VSon2906 <viet29son@gmail.com>
2025-12-08 23:14:48 +0700

    update tile brigde

--- Managers/TileManager.java
+++ Managers/TileManager.java
@@ -118,9 +118,9 @@ public class TileManager {
         tiles.add(GRASS = new Tile(getSprite(atlas2, 1, 5), TileConstant.GRASS));
         tiles.add(SAND = new Tile(getSprite(atlas, 4, 0), TileConstant.SAND));
         tiles.add(WATER = new Tile(getSprite(atlas3,3, 15), TileConstant.WATER));
-        tiles.add(WOOD = new Tile(getSprite(atlas, 22, 11), TileConstant.WOOD));
+        tiles.add(WOOD = new Tile(getSprite(atlas, 0, 3), TileConstant.WOOD));
         tiles.add(HOME = new Tile(getSprite(atlas, 7, 0), TileConstant.HOME));
-        tiles.add(WALL = new Tile(getSprite(atlas, 8, 0), TileConstant.WALL));
+        tiles.add(WALL = new Tile(getSprite(atlas, 5, 8), TileConstant.WALL));
         tiles.add(STONE = new Tile(getSprite(atlas2, 1, 5), TileConstant.STONE));
         tiles.add(CURB = new Tile(getSprite(atlas, 1,3),TileConstant.CURB));
         tiles.add(AVAILABLEDIRT = new Tile(getSprite(atlas, 14,5), TileConstant.AVAILABLEDIRT));
